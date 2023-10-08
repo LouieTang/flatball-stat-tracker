4f54d569d454a50c040e132770b62cd10d3a2472
LouieTang <louietang2013@gmail.com>
2023-10-08 16:34:09 -0700

    working implementation

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ let ourScore = 0;
 let theirScore = 0;
 let gameTotalScore = 0;
 let halfScore = 0;
-let currentCycle = 0;
+let currentState = 0; // 0 - offense, 1 - defense
+let startGender = 0; // 0 - female, 1 - male
+let startState = 0; // 0 - offense, 1 - defense
 
 function startGame(){
     initializeGame();
@@ -21,6 +23,12 @@ function initializeScore(){
     console.log(gameTotalScore);
     halfScore = Math.ceil(gameTotalScore/2);
     console.log(halfScore);
+    updateScore();
+}
+
+function updateScore(){
+    const currentScore = document.getElementById("score");
+    currentScore.innerHTML = `Score: ${ourScore} - ${theirScore}`;
 }
 
 function initializeState(){
@@ -28,8 +36,63 @@ function initializeState(){
     const defense = document.getElementById("defenseStart");
     if(offense.checked){
         stateOffense();
+        currentState = 0;
+        startState = 0;
     }
     else if(defense.checked){
+        stateDefense();
+        currentState = 1;
+        startState = 1;
+
+    }
+}
+
+function updateStateOurScore(){
+    currentState = 1; //set to defense
+    ourScore++;
+    updateGender();
+    updateScore();
+    if(ourScore == gameTotalScore || theirScore == gameTotalScore){
+        finishGame();
+        console.log("here7");
+
+    }
+    if(ourScore == halfScore && theirScore < halfScore){
+        initHalfTime();
+    }
+    else{
+        stateDefense();
+    }
+}
+function updateStateTheirScore(){
+    currentState = 0; //set to still offense
+    theirScore++;
+    updateGender();
+    updateScore();
+    if(theirScore == gameTotalScore || ourScore == gameTotalScore){
+        finishGame();
+        console.log("here6");
+    }
+    if(theirScore == halfScore && ourScore < halfScore){
+        initHalfTime();
+    }
+    else{
+        stateOffense();
+    }
+}
+
+function finishGame(){
+    console.log("here");
+    alert(`Final Score: ${ourScore} - ${theirScore}`);
+    resetPage();
+}
+
+function initHalfTime(){
+    alert("HalfTime");
+    if(startState == 0){
+        stateDefense();
+    }
+    else{
         stateDefense();
     }
 }
@@ -46,28 +109,28 @@ function initializeGame(){
     const mixedGenderRadio = document.getElementById("mixedGender");
 
     if(singleGenderRadio.checked){
-        initilizeNormal();
+        initilizeSingle();
     }
     else if(mixedGenderRadio.checked){
         initializeMixed();
     }
 }
 
-function initilizeNormal(){
-    const currentScore = document.getElementById("score");
-    currentScore.innerHTML = `Score: ${ourScore} - ${theirScore}`;
+function initilizeSingle(){
+    document.getElementById("genderRatio").style.display = "none";
 }
 
 function initializeMixed(){
-    initilizeNormal();
     const currentMatching = document.getElementById("genderRatio");
     const MM = document.getElementById("maleMatching");
     const FM = document.getElementById("femaleMatching");
     if(MM.checked){
         currentMatching.innerHTML = `Current Matching: Male`;
+        startGender = 1;
     }
     else if(FM.checked){
         currentMatching.innerHTML = `Current Matching: Female`;
+        startGender = 0;
     }
 }
 
@@ -88,6 +151,7 @@ function initializePlayers() {
                 goals: 0,
                 assists: 0,
                 callahans: 0,
+                hasDisc: false,
             });
         }
     }
@@ -98,6 +162,7 @@ function initializePlayers() {
         catches: 0,
         completions: 0,
         blocks: 0,
+        hasDisc: false,
     });
 
     console.log(players); // Check for correct functionality
@@ -123,6 +188,7 @@ function closePopup() {
 
 // End Game functionality
 function endGame() {
+    finishGame();
     document.getElementById("endGame").style.display = "none";
 }
 
@@ -134,14 +200,12 @@ function resetPage() {
 
 function stateOffense(){
     const playerListDiv = document.getElementById("playerList");
-    document.getElementById("theirGoal").style.display = "hidden";
+    document.getElementById("theirGoal").style.display = "none";
     playerListDiv.innerHTML = "";
+    let i = 0;
     players.forEach((player) => {
-        playerListDiv.innerHTML += `<div class="player">${player.name} - 
-        <button>Catch</button>
-        <button>Drop</button>
-        <button>Throwaway</button>
-        <button>Goal</button></div>`;
+        playerListDiv.innerHTML += `<div class="player" id="player${i}"><span onclick="hasDisc(${i})">${player.name}</span></div>`;
+        i++
     });
 }
 
@@ -149,10 +213,57 @@ function stateDefense(){
     const playerListDiv = document.getElementById("playerList");
     document.getElementById("theirGoal").style.display = "block";
     playerListDiv.innerHTML = "";
+    let i = 0;
     players.forEach((player) => {
-        playerListDiv.innerHTML += `<div class="player">${player.name} - 
-        <button>Block</button>
-        <button>Block & Score</button>`;
+        playerListDiv.innerHTML += `<div class="player" id="player${i}"><span>${player.name}</span> - 
+        <button onclick="stateOffense()">Block</button>
+        <button onclick="updateStateOurScore()">Block & Score</button>`;
+        i++;
     });
 
 }
+
+function hasDisc(playerNumber){
+    console.log("here?");
+    const playerListDiv = document.getElementById("playerList");
+    document.getElementById("theirGoal").style.display = "none";
+    playerListDiv.innerHTML = "";
+    let i = 0;
+    players.forEach((player) => {
+        if(i != playerNumber){
+            playerListDiv.innerHTML += `<div class="player" id="player${i}"><span">${player.name}</span> - 
+            <button onclick="hasDisc(${i})">Catch</button>
+            <button onclick="stateDefense()">Drop</button>
+            <button onclick="stateDefense()">Throwaway</button>
+            <button onclick="updateStateOurScore()">Goal</button></div>`;
+        }
+        else{
+            playerListDiv.innerHTML += `<div class="player" id="player${i}"><span>${player.name}</span></div>`;
+        }
+        i++
+    });
+}
+
+
+function updateGender(){
+    const currentMatching = document.getElementById("genderRatio");
+    if(startGender == 0){
+        let sumScore = ourScore + theirScore;
+        if(sumScore % 4 == 1 || sumScore % 4 == 2){
+            currentMatching.innerHTML = `Current Matching: Male`;
+        }
+        else{
+            currentMatching.innerHTML = `Current Matching: Female`;
+        }
+    }
+    else if(startGender == 1){
+        let sumScore = ourScore + theirScore;
+        if(sumScore % 4 == 1 || sumScore % 4 == 2){
+            currentMatching.innerHTML = `Current Matching: Female`;
+        }
+        else{
+            currentMatching.innerHTML = `Current Matching: Male`;
+        }
+    }
+}
+
