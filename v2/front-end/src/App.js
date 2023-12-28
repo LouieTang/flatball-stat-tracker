cb68d4c153cd32b96f6e0bb919cc6b1b9ddc80ed
LouieTang <louietang2013@gmail.com>
2023-12-28 12:09:20 -0800

    cleanup, removing unused code from previous version.

--- v2/front-end/src/App.js
+++ v2/front-end/src/App.js
@@ -1,16 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-import TeamsPage from "./pages/TeamsPage.js";
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
-import TeamDataPage from "./pages/TeamDataPage.js";
-import HomePage from "./pages/HomePage.js";
-import AboutPage from "./pages/AboutPage.js";
-import LoginPage from "./pages/LoginPage.js";
-import RegisterPage from "./pages/RegisterPage.js";
-import TeamPage from "./pages/TeamPage.js";
-import CreateTeamPage from "./pages/CreateTeamPage.js";
-import AddPlayersPage from "./pages/AddPlayersPage.js";
-import NewMatch from "./pages/NewMatch.js";
 import TeamHomePage from "./pages/TeamHomePage.js";
 
 function App() {
@@ -18,21 +8,7 @@ function App() {
     <>
         <Header />
         <Routes>
-            {/* <Route path="/" element={<HomePage />} /> */}
             <Route path="/" element={<TeamHomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-
-            <Route path="/team" element={<TeamPage />} />
-            <Route path="/createteam" element={<CreateTeamPage />} />
-            <Route path="/addplayers" element={<AddPlayersPage />} />
-
-            <Route path="/newmatch" element={<NewMatch />} />
-
-
-            <Route path="/teams" element={<TeamsPage />} />
-            <Route path="/teams/:id" element={<TeamDataPage />}/>
         </Routes>
         <Footer />
     </>
--- v2/front-end/src/components/Footer.js
+++ v2/front-end/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="footer">
-        <h5 className="center-text">Version 2.0.0. Updated & Published on December 21st 2023.</h5>
+        <h5 className="center-text">Version 2.0.0. Updated & Published on December 28th 2023.</h5>
         <h5 className="center-text">Copyright &copy; 2023 Louie Tang. All Rights Reserved.</h5>
     </footer>
   )
